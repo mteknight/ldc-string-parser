5168c0ef04b0577161d6e70d98f5d54c5329dd1c
Alexandre Fernandes <mteknight@hotmail.com>
2020-06-11 21:41:20 +0000

    implemented code to remove unwanted characters alonside unwanted duplicates to avoid extra loop.

--- StringParser/StringParser.Services/Parser.cs
+++ StringParser/StringParser.Services/Parser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace StringParser.Services
@@ -18,21 +19,24 @@ namespace StringParser.Services
                 var parsedString = new StringBuilder(stringToParse);
 
                 parsedString.Replace('$', '£');
-                RemoveConsecutiveDuplicates(parsedString);
+
+                RemoveUnwantedOrDuplicatesCharacters(parsedString);
                 parsedString = TruncateString(parsedString);
 
                 yield return parsedString.ToString();
             }
         }
 
-        private static void RemoveConsecutiveDuplicates(StringBuilder parsedString)
+        private static void RemoveUnwantedOrDuplicatesCharacters(StringBuilder parsedString)
         {
+            var unwantedCharacters = new[] { '4', '_' };
+
             for (var i = 1; i < parsedString.Length; i++)
             {
                 var character = parsedString[i];
                 var previousCharacter = parsedString[i - 1];
 
-                if (character == previousCharacter)
+                if (unwantedCharacters.Contains(character) || character == previousCharacter)
                 {
                     parsedString.Remove(i, 1);
                     i--;
